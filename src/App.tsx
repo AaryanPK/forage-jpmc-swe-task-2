a949dd4cf041fc103ad4d1ffead1d76284b98de7
AaryanPK <aaryanpickeringkumar@outlook.com>
2023-08-10 14:34:54 +0100

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,8 @@ class App extends Component<{}, IState> {
   /**
    * Get new data from server and update the state with the new data
    */
+
+  /**
   getDataFromServer() {
     let i = 0;
 
@@ -66,6 +68,23 @@ class App extends Component<{}, IState> {
     if (i > 10000) {
       clearInterval(update);
     }
+  }
+  */
+
+  getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
